96680d4d6f72efb8b4f37b729ae6b735e1846065
Neelesh Nidadhavolu <ganan.neelesh@gmail.com>
2016-06-02 12:17:30 -0400

    int declarations

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,7 +5,14 @@ import (
 	"stringUtils"
 )
 
+var (
+	a = 40
+	b int = 50
+)
+
 func main() {
+	c := 60
 	fmt.Printf(stringUtils.Reverse("\noG ,olleH"))
+	fmt.Printf("%v + %v + %v = %v", a, b, c, a+b+c)
 }
 
